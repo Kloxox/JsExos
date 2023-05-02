89fcc50664ea908385078b00bf6fa59d6e8a7267
Kloxox <90182051+Kloxox@users.noreply.github.com>
2023-05-02 23:04:17 +0200

    Add files via upload

---
+++ exercice1.js
@@ -0,0 +1,17 @@
+var longueur = 0;
+var largeur = 0;
+var resultat1 = 0;
+var resultat2 = 0;
+
+longueur = prompt("la longueur :");
+
+largeur = prompt("la largeur :");
+
+resultat1 = (longueur + largeur) * 2;
+resultat2 = longueur * largeur;
+
+var msg1 = "le résultat du périmètre : ";
+var msg2 = "l’air d’un rectangle : ";
+
+alert(msg1 + resultat1) ;
+alert(msg2 + resultat2) ;
